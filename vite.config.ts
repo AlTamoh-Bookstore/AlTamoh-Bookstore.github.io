ebf605be02ba058dfc828ed8d32cea688859e749
Al-Tamoh <altamohlab00200@gmail.com>
2025-09-01 13:30:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Altamooh-book-store/',
+  base: '/altamoh.bookstore/',
   plugins: [react()],
   build: {
     outDir: 'dist',
@@ -17,3 +17,4 @@ export default defineConfig({
     }
   }
 });
+
