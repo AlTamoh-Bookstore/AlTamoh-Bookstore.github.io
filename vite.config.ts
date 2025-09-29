ec444917ed411ec545f8e89e08f27dae843f6664
Al-Tamoh <altamohlab00200@gmail.com>
2025-09-29 14:54:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/AlTamoh-Bookstore.github.io/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   },
   publicDir: 'public',
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
-  // إضافة هذا للتأكد من معالجة المسارات بشكل صحيح
-  experimental: {
-    renderBuiltUrl(filename: string) {
-      return '/AlTamoh-Bookstore.github.io/' + filename
-    }
-  }
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg']
 });
-
